fdd85e683ab283c59f7636374ef7b3eefd4e3c44
Gavin Macaulay <gavin@macaulay.co.nz>
2022-08-03 12:40:03 +1200

    Several changes:
    - remove xarray use
    - bug fix with cumsum axis
    - optimise code a little

--- dwba/dwba.py
+++ dwba/dwba.py
@@ -13,15 +13,15 @@ Created on Mon Aug  1 12:26:59 2022
 import sys
 import math
 import numpy as np
-import xarray as xr
 from scipy import ndimage
 
 def phase_tracking_dwba(volume, frequencies, voxel_size, angles, densities, sound_speeds):
     """
     
-    volume: 3D numpy array of integer type that contains the object to be modelled.
+    volume: 3D numpy ndarray of integer type that contains the object to be modelled.
             Integers should be 0 for surrounding water, then increasing integers
             for other material types (i.e., 1, 2, 3, etc).
+            This ndarray should be oriented
             
     densities: should be an iterable variable with the same number of entries as
                unique categories in 'volume'  [kg/m^3]
@@ -39,6 +39,7 @@ def phase_tracking_dwba(volume, frequencies, voxel_size, angles, densities, soun
     densities = np.array(densities)
     sound_speeds = np.array(sound_speeds)
     voxel_size = np.array(voxel_size)
+    frequencies = np.array(frequencies)
     
     # volume of the voxels [m^3]
     dv = voxel_size.prod()
@@ -54,16 +55,7 @@ def phase_tracking_dwba(volume, frequencies, voxel_size, angles, densities, soun
     # - that voxel_size values are positive (and identical for the moment)
 
     # an intermediate output data structure
-    fbs_dwba = xr.DataArray(np.zeros((angles.shape[1], len(frequencies)), dtype=np.complex128), 
-                            dims = ('angle','frequency'),
-                            coords = {'pitch': ('angle', angles[0,:]),
-                                      'roll': ('angle', angles[1,:]),
-                                      'frequency': ('frequency', frequencies)})
-    
-    fbs_dwba.frequency.attrs['units'] = 'Hz'
-    fbs_dwba.angle.attrs['units'] = 'degrees'
-    fbs_dwba.angle.attrs['long_name'] = 'Incident direction'
-    fbs_dwba.angle.attrs['description'] = 'Tuple giving direction: [0] is pitch and [1] is roll'
+    fbs_dwba = np.array(np.zeros((angles.shape[1], len(frequencies)), dtype=np.complex128))
 
     g = densities[1:] / densities[0]
     h = sound_speeds[1:] / sound_speeds[0] 
@@ -72,7 +64,7 @@ def phase_tracking_dwba(volume, frequencies, voxel_size, angles, densities, soun
         pitch = angles[0,angle_i]
         roll = angles[1,angle_i]
         
-        print(f'Running at pitch of {pitch}° and roll of {roll}° for {frequencies.min()/1e3} to {frequencies.max()/1e3} kHz')
+        print(f'Running at pitch of {pitch:.1f}° and roll of {roll}° for {frequencies.min()/1e3} to {frequencies.max()/1e3} kHz')
         
         v = volume_rotate(volume, pitch, roll)
         
@@ -94,37 +86,31 @@ def phase_tracking_dwba(volume, frequencies, voxel_size, angles, densities, soun
             masks = []
             for i, category in enumerate(categories):
                 masks.append(np.isin(v, category))
-                #print(masks[i].shape)
-                #print(k[i])
-                #print(voxel_size[0])
                 dph[masks[i]] = k[i] * voxel_size[0]
             masks.pop(0) # don't need to keep the category[0] mask
                 
-            phase = dph.cumsum(axis=2) - dph/2.0 
+            # cummulative summation of phase along the x-direction
+            phase = dph.cumsum(axis=0) - dph/2.0 
             dA = np.zeros(phase.shape, dtype=np.complex128)
     
             # differential phases for each voxel
             for i, m in enumerate(masks):
                 dA[m] = Ca[i] * np.exp(2.0*1j*phase[m]) * dv
             
-            # volume summation
+            # volume summation for total backscatter
             fbs_dwba[angle_i, f_i] = dA.sum()
         
-    # Get the final output data structure
-    ts = 20.0 * np.log10(np.abs(fbs_dwba))
-    ts.attrs['units'] = 'dB'
-    ts.attrs['long_name'] = 'target strength (re 1 m^2)'
+    # Convert to TS
+    return 20.0 * np.log10(np.abs(fbs_dwba))
     
-    return ts
-    
-def volume_rotate(v, theta, phi):
+def volume_rotate(v, pitch, roll):
     """
-    Rotates the 3D ndarray by theta (pitch) and phi (roll) degrees using
+    Rotates the 3D ndarray by pitch and roll degrees using
     nearest neighbour interpolation (to preserve the categorial nature of the 
     data).
 
-    Positive theta pitches head up.
-    Positive phi rolls to the left. 
+    Positive pitch is head up.
+    Positive roll is to the left. 
 
     Returns
     -------
@@ -132,22 +118,24 @@ def volume_rotate(v, theta, phi):
 
     """    
 
-    v = ndimage.rotate(v, -phi, axes=(0,1), order=0)
-    v = ndimage.rotate(v, -theta, axes=(0,2), order=0)
+    v = ndimage.rotate(v, -pitch, axes=(0,2), order=0)
+    v = ndimage.rotate(v, -roll, axes=(0,1), order=0)
 
     return v
 
 
 def main():
     
-    frequencies = np.arange(1000,5001,500)
-    angles = [(0.0, 25.0), (0.0, 45.0)] ######
+    frequencies = np.arange(1000,100001,500)
+    angles = np.array([[0.0, 0.0, 0.0], [0.0, 45.0, -45.0]])
     voxel_size = [5e-4, 5e-4, 5e-4]
     densities = [1024., 1025., 1026.]
     sound_speeds = [1480., 1490., 1500.]
-    volume = np.array([[[0,0,0],[0,1,2],[0,0,0]], 
-                       [[0,0,0],[0,1,2],[0,0,0]], 
-                       [[0,0,0],[0,1,2],[0,0,0]]])
+    volume = np.array([[[0,0,0,0],[0,1,2,0],[0,0,0,0]], 
+                       [[0,0,0,0],[0,3,4,0],[0,0,0,0]], 
+                       [[0,0,0,0],[0,5,6,0],[0,0,0,0]],
+                       [[0,0,0,0],[0,7,8,0],[0,0,0,0]],
+                       [[0,0,0,0],[0,9,10,0],[0,0,0,0]]])
     
     ts = phase_tracking_dwba(volume, frequencies, voxel_size, angles, densities, sound_speeds)
     
