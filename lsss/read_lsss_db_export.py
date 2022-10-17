8f7fc31e471cb70a49bcf9bbfe3bfbd9bce71e1f
Gavin Macaulay <gavin@macaulay.co.nz>
2022-10-18 11:50:13 +1300

    Improvements
    Support selecting frequency to extract
    Return channel min and max depth
    Use named instead of numbered fields when extract data from xml

--- lsss/read_lsss_db_export.py
+++ lsss/read_lsss_db_export.py
@@ -11,10 +11,12 @@ import datetime as dt
 import pandas as pd
 import numpy as np
 
-def read_lsss_db_export(format, filename, acocat):
+def read_lsss_db_export(format, filename, acocat, frequency):
     """
     acocat is always converted to a string
     format should be integer
+    
+    frequency should be integer in Hz
 
     Parameters
     ----------
@@ -41,10 +43,13 @@ def read_lsss_db_export(format, filename, acocat):
         channel_acocat = []
         channel_dist = []
         channel_thickness = []
+        channel_min_depth = []
+        channel_max_depth = []
         
         acocat = str(acocat)
+        frequency = str(frequency)
         
-        acolist = root[6]
+        acolist = root.find('acocat_list')
         
         categories = []
         for c in acolist:
@@ -54,16 +59,15 @@ def read_lsss_db_export(format, filename, acocat):
             print(f'Provided acocat ({acocat}) is not in this xml file!')
             
         for n in root.iter('distance'): # loop over each integration interval
-            thickness = float(n[2].text)
-            distance = float(n[1].text) # [nmi]
+            thickness = float(n.find('pel_ch_thickness').text)
+            distance = float(n.find('integrator_dist').text) # [nmi]
             start_time = dt.datetime.strptime(n.attrib['start_time'], '%Y-%m-%d %H:%M:%S')
             #print(f'Reading data at ping time of {start_time}')
-            start_lat = float(n[5].text)
-            start_lon = float(n[7].text)
-            ch = n[9]
-            if ch.attrib['freq'] == '38000':                
-                channel_start_depth = float(ch[5].text)
-                num_channels = int(ch[1].text)
+            start_lat = float(n.find('lat_start').text)
+            start_lon = float(n.find('lon_start').text)
+            ch = n.find('frequency')
+            if ch.attrib['freq'] == frequency:                
+                num_channels = int(ch.find('num_pel_ch').text)
                 sA_values = np.zeros((num_channels,))
                 channel_time.append(start_time)
                 channel_lat.append(start_lat)
@@ -71,8 +75,11 @@ def read_lsss_db_export(format, filename, acocat):
                 channel_dist.append(distance)
                 channel_thickness.append(thickness)
                 channel_acocat.append(acocat)
-                if (ch[11].attrib['type'] == 'P'): # pelagic data
-                    for sA in ch[11]: # loop over all acoustic categories
+                channel_min_depth.append(float(ch.find('upper_interpret_depth').text))
+                channel_max_depth.append(float(ch.find('lower_interpret_depth').text))
+                channel_type = ch.find('ch_type')
+                if (channel_type.attrib['type'] == 'P'): # pelagic data
+                    for sA in channel_type: # loop over all acoustic categories
                         if sA.attrib['acocat']  == acocat: # if we find the specified acoustic category
                             for child in sA: # loop over all sa values
                                 channel_id = int(child.attrib['ch'])
@@ -82,6 +89,10 @@ def read_lsss_db_export(format, filename, acocat):
  
     
         # combine into a DataFrame 
-        r = pd.DataFrame(zip(channel_time, channel_lat, channel_lon, channel_sA_sum, channel_sA, channel_acocat, channel_dist, channel_thickness), 
-                         columns=['time', 'lat', 'lon', 'sA_sum', 'sA', 'acocat', 'distance', 'thickness'])
+        r = pd.DataFrame(zip(channel_time, channel_lat, channel_lon, channel_sA_sum, 
+                             channel_sA, channel_acocat, channel_dist, channel_thickness,
+                             channel_min_depth, channel_max_depth), 
+                         columns=['time', 'lat', 'lon', 'sA_sum', 'sA', 'acocat', 
+                                  'distance', 'thickness', 'channel_min_depth',
+                                  'channel_max_depth'])
     return r
