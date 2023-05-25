9d31831355b996dbad431d167b84f7dd4ce6cb6a
Gavin Macaulay <gavin@macaulay.co.nz>
2023-05-25 14:17:19 +1200

    Better mask to polygon detection, plot categorised regions

--- lsss/read_korona_datagrams.py
+++ lsss/read_korona_datagrams.py
@@ -13,13 +13,25 @@ import pandas as pd
 import sys
 import xarray as xr
 import numpy.ma as ma
-
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from matplotlib.patches import Polygon, Patch
+from datetime import datetime, timedelta
+
+from rasterio import features
+import rasterio
+import shapely
 
 sys.path.append(r'C:\Users\gavin\Data - not synced\Code\python\lsss')
 
 from ek_raw_io import RawSimradFile, SimradEOF
 from pathlib import Path
+
+projectDir = Path(r'C:\Users\gavin\OneDrive - Aqualyd Limited\Documents\Aqualyd\Projects\2023-01 CSIRO inversion')
+dataDir = projectDir/'data'/'LSSS_DATA'/'KORONA'
+resultsDir = projectDir/'results'
+
 
 # Take the korona datagrams and filter the plankton categories by korona regions
 def filter_cat_by_regions(rbr, rnf, pid, raw, cat_names):
@@ -37,26 +49,32 @@ def filter_cat_by_regions(rbr, rnf, pid, raw, cat_names):
     raw_depths = np.arange(raw[0]['count'])
     sample_int = raw[0]['sample_interval'] * raw[0]['sound_velocity']/2 # [m]
     
-    plankton_cats = xr.DataArray(0, coords=[raw_depths, raw_timestamps], dims=['depth', 'time'])
+    # 2D array to store plankton categories - covers entire raw file at sample resolution
+    pc = xr.DataArray(0, coords=[raw_depths, raw_timestamps], dims=['depth', 'time'])
     
     # and now populate plankton_cats with the actual categories
     # this is inefficient and takes a long time to run
+    print('    Creating category dataset')
     for p in pid: # for each category datagram
+        #print(f'{ii} of {len(pid)}')
+        time_i = np.argwhere(pc.time.data==p['timestamp'])[0][0]
         sample_starts = [s['start_sample_number'] for s in p['plankton_samples'].values()]
         sample_ends = np.array(sample_starts[1:]+[len(raw_depths)])
         for s, s_start, s_end in zip(p['plankton_samples'].values(), sample_starts, sample_ends):
             for d in s['plankton_data'].values(): # for each data
-                mask = dict(time=p['timestamp'], depth=raw_depths[(raw_depths>=s_start)&(raw_depths<s_end)])
                 if d['plankton_number'] > 0:
-                    plankton_cats.loc[mask] = d['plankton_number']
-    
+                    pc[s_start:s_end, time_i] = d['plankton_number']
+
     # now convert the depth axis from samples to metres
-    plankton_cats['depth'] = raw_depths * sample_int
+    pc['depth'] = raw_depths * sample_int
     
     # for each region in the file, find the plankton categories inside it
     rc = {'category': [], 'timestamp': [], 'mid_depth': [], 'area': [], 'height': [],
-          'mean_Sv': []}
+          'mean_Sv': [], 'width': []}
     
+    region_polygon = {v:[] for v in cat_names.values()}
+    
+    print(f'    Filtering by regions ({len(rnf)})')
     for region in rnf:
         ids = region['border_ids']
         b = region_borders.query('id in @ids')
@@ -76,8 +94,8 @@ def filter_cat_by_regions(rbr, rnf, pid, raw, cat_names):
             region_mask.loc[mask] = False
         
         # now extract the region_mask bounding box of the planton categories
-        rr = plankton_cats.sel(time=region_mask.time, depth=region_mask.depth, method='nearest')
-        # and select out the plankton_cats where region_mask is False
+        rr = pc.sel(time=region_mask.time, depth=region_mask.depth, method='nearest')
+        # and select out the pc where region_mask is False
         bins = np.bincount(ma.masked_array(rr, mask=region_mask).data.flatten())
         categories = np.arange(len(bins))
         
@@ -88,31 +106,50 @@ def filter_cat_by_regions(rbr, rnf, pid, raw, cat_names):
         rc['area'].append(region['area'])
         rc['height'].append(region['bb_height'])
         rc['mean_Sv'].append(region['log_mean_Sv'])
+        rc['width'].append(region['bb_width'])
+        
+        # generate a polygon version of the region and tag with category
+        time_axis = np.array(region_mask.time)
+        time_axis = np.append(time_axis, time_axis[-1]+np.diff(time_axis[-2:]))
+        
+        depth_axis = np.array(region_mask.depth)
+        depth_axis = np.append(depth_axis, depth_axis[-1]+np.diff(depth_axis[-2:]))
+        
+        if (region_mask.shape[0] > 2) and (region_mask.shape[1] > 2):
+            for shape, value in features.shapes(region_mask.astype(np.int16), mask=(region_mask<1), transform=rasterio.Affine(1.0, 0, 0, 0, 1.0, 0)):
+                outer = np.array(shape['coordinates'][0])
+                outer[:,0] = time_axis[outer[:,0].astype('int')]
+                outer[:,1] = depth_axis[outer[:,1].astype('int')]
+                region_polygon[rc['category'][-1]].append(outer)
             
     region_classification = pd.DataFrame(data=rc)
     region_classification.set_index('timestamp', inplace=True)
 
-    return region_classification
+    return region_classification, region_polygon
 
 #%%
 # Process the CSIRO example data
 
-projectDir = Path(r'C:\Users\gavin\OneDrive - Aqualyd Limited\Documents\Aqualyd\Projects\2023-01 CSIRO inversion')
-dataDir = projectDir/'data'/'LSSS_DATA'/'KORONA'
-resultsDir = projectDir/'results'
-
 koronaFiles = dataDir.glob('*.raw')
-# for testing, select just one file
-#koronaFiles=list(koronaFiles)[0:1]
+# for testing, select some files
+koronaFiles=list(koronaFiles)[5:]
 
-pid = []
-rbr = []
-rnf = []
-raw = []
+import logging
+logger = logging.getLogger('')
+logger.setLevel(logging.ERROR)
+
 
 for f in koronaFiles:
     print('Processing file ' + f.name)
+    
     with RawSimradFile(str(f), "r", storage_options={}) as fid:
+        pid = []
+        rbr = []
+        rnf = []
+        raw = []
+
+        print('    Reading file')
+
         config_datagram = fid.read(1)
         config_datagram["timestamp"] = np.datetime64(config_datagram["timestamp"].replace(tzinfo=None), "[ns]")
        
@@ -136,6 +173,7 @@ for f in koronaFiles:
             if datagram['type'] == 'RAW0':
                 raw.append(datagram)
         fid.close()
+        print(f'    File has {len(raw)} RAW datagrams')
         
         # Now process these datagrams to filter the plankton categories from Korona
         # by the Korona detected school regions.
@@ -146,17 +184,122 @@ for f in koronaFiles:
         cat_names[1] = 'Other'
 
         # do the actual filtering. Returns a DataFrame
-        rc = filter_cat_by_regions(rbr, rnf, pid, raw, cat_names)
+        rc, rp = filter_cat_by_regions(rbr, rnf, pid, raw, cat_names)
         rc.to_csv(resultsDir/(f.stem+'.csv'))
+        np.save(resultsDir/(f.stem+'-polygons.npy'), rp)
 
 #%%
-# now report on those region category stats
-cats, counts = np.unique(rc.category, return_counts=True)
-plt.bar(cats, counts)
-plt.ylabel('Number of regions')
 
-rc.hist(column='mid_depth', density=False, rwidth=0.8)
-plt.xlabel('Region depth [m]')
-plt.ylabel('Number of regions')
+# load in all the .csv results files into one DataFrame and create some plots
 
+resultsFiles = resultsDir.glob('*-korona.csv')
+rc_full = []
+for r in resultsFiles:
+    print('Loaded ' + r.name)
+    rc_full.append(pd.read_csv(r, index_col='timestamp', parse_dates=True))
+    
+rc_full = pd.concat(rc_full)
+rc_full['timeofday'] = [(t+timedelta(hours=10)).replace(year=2012,month=1,day=1) for t in rc_full.index]
+
+# plots of region size distributions
+
+for i, c in enumerate(rc_full.category.unique()):
+    fig, axs = plt.subplots(1,4, layout='constrained')
+    rc_full.query('category == @c').hist(column='height', bins=25, ax=axs[0])
+    rc_full.query('category == @c').hist(column='width', bins=25, ax=axs[1])
+    rc_full.query('category == @c').hist(column='area', bins=25, ax=axs[2])
+    rc_full.query('category == @c').plot(x='width', y='height', ax=axs[3], kind='scatter')
+    fig.suptitle(c)
+    plt.close(fig)
+    fig.savefig(resultsDir.joinpath(f'Figure_region_stats_{c}.png'), dpi=600, bbox_inches='tight', pad_inches=0.1)
+
+# exclude small regions. the above histograms were used to set the values here:
+rc = rc_full.query('(category == "FBCyl"  and height > 2) or'
+                   '(category == "FluidS" and height > 2) or'
+                   '(category == "Gas") or'
+                   '(category == "Hard"   and height > 2 and width > 5) or'
+                   '(category == "Other"  and height > 1 and width > 4) or'
+                   '(category == "Uncat"  and height > 2 and width > 50)')
+#rc = rc_full
+
+minSv = np.round(rc.mean_Sv).unique().min()
+maxSv = np.round(rc.mean_Sv).unique().max()
+cmap = mpl.cm.plasma
+
+# group the plots by year/month
+group_fmt = '%Y-%m'
+groups = rc.index.strftime(group_fmt).unique()
+
+
+# do a separate figure for each category
+cats = rc.category.unique()
+for c in cats:
+    print(f'Doing category {c}')
+    fig, axs = plt.subplots(4,5, sharey=True, sharex=True, layout='constrained')
+    for ax, group in zip(axs.flat, groups):
+        chunk = rc[(rc.index.strftime(group_fmt) == group) & (rc.category == c)]
+        rcc = chunk
+        sc = ax.scatter(rcc.timeofday, rcc.mid_depth, rcc.area, 
+                        vmin=minSv, vmax=maxSv, cmap=cmap,
+                        c=rcc.mean_Sv)
+        ax.annotate(f'{group}', 
+                     xy=(0.02, 0.99), xycoords='axes fraction', ha='left',
+                     va='top', fontsize=6)
+    
+    # Adjust the labelling
+    for ax in axs[-1,:]:
+        ax.tick_params(axis='x', labelsize=6, rotation=45)
+        ax.xaxis.set_major_formatter(mdates.DateFormatter('%H'))
+        
+    # colorbar for colour of circles
+    norm = mpl.colors.Normalize(vmin=minSv, vmax=maxSv)
+    cbar = fig.colorbar(mpl.cm.ScalarMappable(norm=norm, cmap=cmap),
+                        ax=axs[0,-1], orientation='vertical', label='$S_v$')
+    cbar.ax.tick_params(labelsize='xx-small')
+    # legend for size of circles
+    #handles, labels = sc.legend_elements(prop="sizes")
+    #fig.legend(handles, labels, loc='outside center right', title='Area')
+    
+    fig.supylabel('Range [m]', fontsize=10)
+    fig.supxlabel('Time of day (UTC+10:00)', fontsize=10)
+    fig.suptitle(c)
+    plt.close(fig)
+    fig.savefig(resultsDir/f'Figure_bubbles_{c}.png', dpi=600, bbox_inches='tight', pad_inches=0.1)   
+
+#%%
+# and a category echogram of the entire dataset
+
+#colours = {v:f'C{i+1}' for i, v in enumerate(cat_names.values())}
+colours={'Hard':'C1','Gas':'C2', 'FluidS':'C3', 'FBCyl':'C4', 'Uncat':'C5', 'Other':'C6'}
+
+rpFiles = resultsDir.glob('*-korona-polygons.npy')
+for i, rpf in enumerate(rpFiles):
+    print('Loading ' + rpf.name)
+    rp = np.load(rpf, allow_pickle=True)
+    rp = rp[()]
+
+    fig, ax = plt.subplots(1, layout='constrained')
+    for cat,regions in rp.items():
+        cc = colours[cat]
+        for r in regions:
+            t, d = zip(*r)
+            d = 91 - np.array(d) # convert to depth below surface
+            t = mdates.date2num(np.array(t, dtype='datetime64[ns]'))
+            #ax.plot(t, d, c=cc)
+            ax.plot(t[0],d[0],c=cc) # avoids a bug where add_patch doesn't update the axis limits to fit the data
+            
+            polygon = Polygon((np.array((t,d))).T, closed=True, 
+                              edgecolor=cc, facecolor=cc)
+            ax.add_patch(polygon)
+
+    patches = [Patch(color=c, label=l) for l, c in colours.items()]
+    plt.legend(patches, colours.keys(), loc='center left', 
+               bbox_to_anchor=(1.04,0.5), frameon=False)
+    
+    ax.tick_params(axis='x', labelsize=6, rotation=45)
+    ax.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m-%d'))
+    ax.invert_yaxis()
+    ax.set_ylabel('Depth [m]')
+    fig.savefig(resultsDir/f'Figure_categorised_echogram_{i+1:02d}.png', dpi=600, bbox_inches='tight', pad_inches=0.1)   
+    plt.close(fig)
 
