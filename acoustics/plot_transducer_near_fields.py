0c405875913ccd86c2388eb591bc08718964354c
Gavin Macaulay <gavin@macaulay.co.nz>
2024-02-14 09:45:27 +1300

    Use labellines package to do line labels

--- acoustics/plot_transducer_near_fields.py
+++ acoustics/plot_transducer_near_fields.py
@@ -8,6 +8,7 @@ transduer beamwidth and acoustic frequency.
 
 import matplotlib.pyplot as plt
 import numpy as np
+from labellines import labelLines # see https://github.com/cphyc/matplotlib-label-lines
 
 c = 1470. # [m/s] speed of sound in water
 freq = np.array([12, 18, 38, 70, 120, 200, 333]) * 1e3 # [Hz] acoustic frequency
@@ -28,22 +29,8 @@ for f in freq:
     r_nf  = 3 * np.pi * d_t**2.0 / (4.0 * lambda_) # [m]
 
     line = ax.plot(bw, r_nf, color='black', linewidth=2, label=f'{f/1000:.0f} kHz')
-    
-    # add text for the frequency to the line
-    label = f'{f/1000:.0f} kHz'
-    
-    idx = np.argwhere(np.diff(np.sign(textLine - r_nf))).flatten()[0]
-    npts = 1
 
-
-    p1 = ax.transData.transform_point((bw[idx-npts], r_nf[idx-npts]))
-    p2 = ax.transData.transform_point((bw[idx+npts], r_nf[idx+npts]))
-    dy = ((p2[1] - p1[1]))
-    dx = (p2[0] - p1[0])
-    rotn = np.degrees(np.arctan2(dy, dx))
-    ax.text(bw[idx-npts], r_nf[idx-npts], label, fontsize=7,
-            rotation=rotn, rotation_mode='anchor',
-            transform_rotates_text=True, va='bottom')
+labelLines(ax.get_lines(), fontsize=7, xvals=(16,2))
 
 plt.minorticks_on()
 ax.grid(axis='x', which='minor', linewidth=0.5, linestyle=':')
@@ -54,11 +41,5 @@ ax.set_yticks(yticksAt)
 ax.set_yticklabels((str(i) for i in yticksAt))
 ax.set_ylim(yticksAt[0], yticksAt[-1])
 ax.set_xlim(bw[0], bw[-1])
-ax.set_xlabel('Transducer 3 dB beamwidth [$\degree$]')
+ax.set_xlabel('Transducer 3 dB beamwidth [°]')
 ax.set_ylabel('Minimum sphere range [m]')
-
-# add labels to the lines. No easy way to do this with matplotlib that I know of...
-
-# for each line, find the middle of the visible data and place the text there
-# also rotate the text to be in line with the line
-# and have a transparent background to the text OR sit above the line
