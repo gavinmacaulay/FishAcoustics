45dab3282b00b8315d6c83dc577d832f8028504e
Gavin Macaulay <gavin@macaulay.co.nz>
2024-02-25 21:47:28 +1300

    Fix some pylint warnings

--- src/acoustics/plot_transducer_near_fields.py
+++ src/acoustics/plot_transducer_near_fields.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-Creates a plot of recommended minimum sphere calibration range given a
+Creates a plot of recommended minimum sphere calibration range as per
 transduer beamwidth and acoustic frequency.
 
 @author: Gavin Macaulay
@@ -8,29 +8,26 @@ transduer beamwidth and acoustic frequency.
 
 import matplotlib.pyplot as plt
 import numpy as np
-from labellines import labelLines # see https://github.com/cphyc/matplotlib-label-lines
+from labellines import labelLines  # see https://github.com/cphyc/matplotlib-label-lines
 
-c = 1470. # [m/s] speed of sound in water
-freq = np.array([12, 18, 38, 70, 120, 200, 333]) * 1e3 # [Hz] acoustic frequency
-bw = np.linspace(2,20,50) # [deg] 3 dB two-way beamwidth
+c = 1470.  # [m/s] speed of sound in water # pylint: disable=invalid-name
+freq = np.array([12, 18, 26, 38, 50, 70, 120, 200, 333]) * 1e3  # [Hz] acoustic frequency
+bw = np.linspace(2, 20, 50)  # [deg] 3 dB two-way beamwidth
 
 yticksAt = [1, 1.5, 2, 3, 4, 6, 8, 10, 15, 20]
 
-textLine = (yticksAt[-1]-yticksAt[0])/(bw[-1]-bw[0]) * 10**((bw-bw[0])/10) + yticksAt[0]
-
-#%%
 fig, ax = plt.subplots(1)
 
 for f in freq:
-    lambda_ = c / f # [m]
-    k = 2 * np.pi * f / c # [m-1]
+    lambda_ = c / f  # [m]
+    k = 2 * np.pi * f / c  # [m-1]
 
-    d_t = 3.2 / (k * np.sin(np.deg2rad(bw/2.0))) # [m]
-    r_nf  = 3 * np.pi * d_t**2.0 / (4.0 * lambda_) # [m]
+    d_t = 3.2 / (k * np.sin(np.deg2rad(bw/2.0)))  # [m]
+    r_nf = 3 * np.pi * d_t**2.0 / (4.0 * lambda_)  # [m]
 
     line = ax.plot(bw, r_nf, color='black', linewidth=2, label=f'{f/1000:.0f} kHz')
 
-labelLines(ax.get_lines(), fontsize=7, xvals=(16,2))
+labelLines(ax.get_lines(), fontsize=7, xvals=(16, 2))
 
 plt.minorticks_on()
 ax.grid(axis='x', which='minor', linewidth=0.5, linestyle=':')
