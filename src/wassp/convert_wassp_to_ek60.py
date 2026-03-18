c553c9e1448e3ecd36674e1d9b5328fa5093e7b0
Gavin Macaulay <gavin@macaulay.co.nz>
2026-03-19 09:35:24 +1300

    Can run as a command line program now

--- src/wassp/convert_wassp_to_ek60.py
+++ src/wassp/convert_wassp_to_ek60.py
@@ -1,30 +1,47 @@
-"""Convert WASSP files into EK60-formatted files.
+"""Convert WASSP multibeam files into EK60-formatted files.
 
 Extracts the echosounder data and writes out as Simrad EK60 data files.
-If desired, multiple echosounder beams get saved as multiple
-channels in the EK60 files. Plenty of hard-coded parameters here to make things
-work enough for the purpose.
+
+There are plenty of hard-coded parameters here to make things work enough for the purpose.
 """
-# /// 
+# ///
 # dependencies = [
 #   "numpy"
 # ]
 # ///
 
+import argparse
 from pathlib import Path
 import numpy as np
 import struct
 from dataclasses import dataclass
 from datetime import datetime, timedelta
 
-project_dir = Path(r'D:\Working\Projects\Active\2022 WindFarms')
-dataDir = project_dir/'data'/'wassp_files'
+parser = argparse.ArgumentParser(description='Converts WASSP multibeam files into EK60-formatted files.',
+                                 epilog='If no input directory is given, '\
+                                     'the current directory is used.\n'
+                                 'If no output directory is given, the EK60 files are written '\
+                                     'to the same directory as the WASSP files.',
+                                 formatter_class=argparse.RawDescriptionHelpFormatter)
+parser.add_argument("-i", "--input", help="Directory to look for WASSP files.", default='.')
+parser.add_argument("-o", "--output", help="Directory write EK60 files to.")
+parser.add_argument("-v", "--verbose", help='Provide more information during conversion.',
+                    action='store_true')
+args = parser.parse_args()
 
-# the ek60 .raw files can either go into the same directory as the wassp files 
-# if resultsDir is None, or into a common directory, specified here:
-results_dir = project_dir/'data'/'ek60_files'
+data_dir = Path(args.input)
 
-files = sorted(dataDir.glob('**/*.wmbf'))
+if not args.output:
+    results_dir = Path(args.input)
+else:
+    results_dir = Path(args.output)
+
+verbose = args.verbose
+
+files = sorted(data_dir.glob('**/*.wmbf'))
+
+if verbose:
+    print(f'Found {len(files)} file(s) in directory {data_dir}')
 
 default_gain = 13 # [dB]
 default_pulse_duration = 0.001 # [s]
@@ -36,7 +53,7 @@ epoch = datetime(1601, 1, 1)
 # Define various classes and functions
 def datetimeToEK60Time(wasspTime):
     """Convert Python datetime to EK60 time.
-    
+
     EK60 time is the same as Windows NT time, so 100 nanoseconds since January 1, 1601
     as a 64 bit unsigned integer.
     """
@@ -44,47 +61,47 @@ def datetimeToEK60Time(wasspTime):
 
 def datetimeFromWasspTime(ping_date, timeofday):
     """Generate a Python datetime from the given values.
-    
+
     Timeofday is in nanoseconds since midnight
     """
     return ping_date + timedelta(microseconds=timeofday*1e-3)
 
 class EK60datagram(object):
-    
+
     def __init__(self, dg_type, dg_timestamp: datetime):
         self.headerlength = 12
         self.lengthlength = 8 # 4 bytes at each end
         self.type = dg_type
         self.timestamp = dg_timestamp
         self.length = 0
-        
+
     def write_header(self, fid):
         #print(f'Writing datagram of {self.length} bytes')
         fid.write(struct.pack('<l', self.length))
         fid.write(self.type.encode())
         fid.write(struct.pack('<Q', datetimeToEK60Time(self.timestamp)))
-        
+
     def write_trailer(self, fid):
         fid.write(struct.pack('<l', self.length))
-        
+
     def __str__(self):
         return f'Datagram of type {self.type} and length {self.length} bytes.'
-   
+
 class EK60nmea(EK60datagram):
     def __init__(self, dg_timestamp, nmea):
         EK60datagram.__init__(self, 'NME0', dg_timestamp)
         self.nmea = nmea.strip()
         self.length = self.headerlength + len(self.nmea)
-        
+
     def write(self, fid):
-        self.write_header(fid)        
+        self.write_header(fid)
         fid.write(self.nmea.encode('ASCII'))
         self.write_trailer(fid)
 
 class EK60raw(EK60datagram):
     def __init__(self, dg_timestamp, raw):
         EK60datagram.__init__(self, 'RAW0', dg_timestamp)
-        
+
         self.channel = 0
         self.mode = 1 # power data only
         self.transducerdepth = 0.0
@@ -106,9 +123,9 @@ class EK60raw(EK60datagram):
         self.offset = 0
         self.count = len(raw)
         self.power = np.round(raw / (10*np.log10(2.0)/256.0)).astype('short')
-        
+
         self.length = self.headerlength + 72 + 2*len(self.power)
-    
+
     def write(self, fid):
         # a=fid.tell()
         self.write_header(fid)
@@ -116,18 +133,18 @@ class EK60raw(EK60datagram):
                               self.frequency, self.transmitpower, self.pulselength,
                               self.bandwidth, self.sampleinterval, self.soundspeed,
                               self.absorptioncoefficient, self.heave, self.txroll,
-                              self.txpitch, self.temperature, self.spare1, self.spare2, 
+                              self.txpitch, self.temperature, self.spare1, self.spare2,
                               self.rxroll, self.rxpitch, self.offset, self.count))
         self.power.tofile(fid)
         self.write_trailer(fid)
         # print(f'{fid.tell()-a-self.lengthlength}, cf {self.length}')
-        
+
 @dataclass
 class EK60configtransducer(object):
     channel: str = format('WASSP echosounder', '\x00<128') # placeholder
     beamtype: int = 0
     frequency: float = default_frequency # [Hz] placeholder
-    gain: float = default_gain # [dB] 
+    gain: float = default_gain # [dB]
     equivalentbeamangle: float = -20.0 # [dB] placeholder
     bwalongship: float = 0.0
     bwathwartship: float = 0.0
@@ -136,7 +153,7 @@ class EK60configtransducer(object):
     offsetalongship: float = 0.0
     offsetathwartship: float = 0.0
     pulselength = np.array([1,2,3,4,5], dtype='float32')*default_pulse_duration
-    spare1: str = ' '*8 
+    spare1: str = ' '*8
     gaintable = np.full(5, default_gain, dtype='float32')
     spare2: str = ' '*8
     sacorrection = np.full(5, 0.0, dtype='float32')
@@ -144,15 +161,15 @@ class EK60configtransducer(object):
     gptsoftwareversion: str = ' '*16
     spare4: str = ' '*28
     length: int = 320 # bytes
-    
+
     def setChannelName(self, name):
         self.channel = format(name[:128], '\x00<128')
-        
+
     def write(self, fid):
         bytesWritten = fid.tell()
         fid.write(self.channel.encode())
         fid.write(struct.pack('<lfffffffff', self.beamtype, self.frequency, self.gain,
-                               self.equivalentbeamangle, self.bwalongship, 
+                               self.equivalentbeamangle, self.bwalongship,
                                self.bwathwartship, self.asalongship, self.asathwartship,
                                self.offsetalongship, self.offsetathwartship))
         fid.write(struct.pack('<ffffff', 0.0, 0.0, 0.0, 0.0, 0.0, 0.0))
@@ -166,7 +183,7 @@ class EK60configtransducer(object):
         fid.write(self.spare4.encode())
         bytesWritten = fid.tell() - bytesWritten
         #print(f'configtransducer: {bytesWritten}, cf {self.length}')
-        
+
 @dataclass
 class EK60configheader(object):
     surveyname: str = ' '*128
@@ -176,7 +193,7 @@ class EK60configheader(object):
     spare: str = ' '*98
     transducercount: int = 1
     length: int = 516 # bytes
-    
+
     def write(self, fid):
         bytesWritten = fid.tell()
         fid.write(self.surveyname.encode())
@@ -188,12 +205,12 @@ class EK60configheader(object):
         bytesWritten = fid.tell() - bytesWritten
         # print(f'configheader: {bytesWritten}, cf {self.length}')
 
-    
+
 class EK60configuration(EK60datagram):
     def __init__(self, dg_timestamp):
         EK60datagram.__init__(self, 'CON0', dg_timestamp)
         self.initialised = False
-        
+
     def initialise(self, numchannels):
         if not self.initialised:
             self.initialised = True
@@ -205,8 +222,8 @@ class EK60configuration(EK60datagram):
                 self.length += self.configtransducer[i].length
             for i, t in enumerate(self.configtransducer):
                 t.setChannelName(f'WASSP echosounder beam {i+1}')
-                
-        
+
+
     def write(self, fid):
         bytesWritten = fid.tell()
         self.write_header(fid)
@@ -223,12 +240,10 @@ class EK60configuration(EK60datagram):
 prev_file_ping_time = epoch
 
 for file in files:
-    print(f'Processing {file.name}')
+    if verbose:
+        print(f' Processing {file.name}')
 
     packet_types = []
-
-    echogram = []
-    plot_done = False
 
     ek60_config = EK60configuration(epoch)
     ek60_datagrams = [ek60_config]
@@ -236,16 +251,16 @@ for file in files:
     haveValidDate = False
     ping_date = epoch
     prev_timeofday = -1
-    
+
     sonar_freq = []
     sonar_bandwidth = []
-    
+
     with open(file, mode='rb') as f:
         while True:
             packet_start = f.read(4)
             if not packet_start: # eof
                 break
-        
+
             magic_start, = struct.unpack('<L', packet_start)
             length, = struct.unpack('<L', f.read(4))
             packet_type = struct.unpack('8s', f.read(8))[0].decode('ascii')
@@ -253,31 +268,31 @@ for file in files:
             msg_flags, = struct.unpack('<L', f.read(4))
             packet = f.read(length - 28)
             magic_end, = struct.unpack('<L', f.read(4))
-        
+
             packet_types.append(packet_type)
             #print(f'Read packet: {magic_start:X}, {length}, {packet_type}, {magic_end:X}')
             ii += 1
             # if ii > 50:
             #    break
-                
+
             if packet_type == 'SONADISP':
                 i = 92
-                # (_, timestamp, pingno, lat, long, bearing, sample_rate, c, 
-                #  alpha, spreading, N, M, tx_power, pulse_width, sample_type, 
+                # (_, timestamp, pingno, lat, long, bearing, sample_rate, c,
+                #  alpha, spreading, N, M, tx_power, pulse_width, sample_type,
                 #  sample_offset, _, _, _) = struct.unpack('<QQIddfffffIIfIIIIII', packet[:i])
                 # # sort out some units
                 # pulse_width = pulse_width * 1e-9 # [s]
-                
+
                 # i += N*4 # skip over N reserved U32's
-                
+
                 # detection_point = np.frombuffer(packet[i:], dtype='uint32', count=N)
                 # i += N*4
-                
+
                 # beam_angle = np.frombuffer(packet[i:], dtype='float32', count=N)
                 # i += N*4
-                
+
                 # sample_interval = c / sample_rate / 2 # [m]
-                
+
                 # data = np.frombuffer(packet[i:], dtype='int16', count=N*M)
                 # data = np.reshape(data, (N,M)).T / 128.0 # [dB]
 
@@ -293,14 +308,14 @@ for file in files:
                 #     plt.savefig(dataDir.joinpath(f'{file.stem}_sonar_{pingno}.png'), bbox_inches='tight', pad_inches=0.1)
                 #     plt.close()
                 #     plot_done = True
-                
+
             if packet_type == 'SONASTAT':
                 i = 48
                 (_, system_temp, transducer_temp, ping_rate, freq, bandwidth, ping_state,
                  c, tide, link_speed, progress, source, status) = struct.unpack('<QfffffIffIBBH', packet[:i])
                 sonar_freq.append(freq)
                 sonar_bandwidth.append(bandwidth)
-                
+
             if packet_type == 'SENUPDAT':
                 i = 16
                 (_, year, month, day, hour, minute, millisecond) = struct.unpack('<QHBBBBH', packet[:i])
@@ -310,46 +325,46 @@ for file in files:
                     ek60_config.timestamp = datetimeFromWasspTime(ping_date, (hour*60*60 + minute*60)*1e9 + millisecond*1e6)
                     haveValidDate = True
                 # print(f'SENUPDAT {ek60_config.timestamp:%Y-%m-%d %H:%M:%S.%f}')
-                                
+
             if packet_type == 'FISHDATA':
                 if haveValidDate:
                     i = 84
-                    
-                    (_, timeofday, pingno, lat, long, bearing, sample_rate, c, 
+
+                    (_, timeofday, pingno, lat, long, bearing, sample_rate, c,
                      alpha, spreading, N, M, sample_type, _, _, _, _) = struct.unpack('<QQIddfffffIIIIIII', packet[:i])
-    
+
                     if timeofday < prev_timeofday:
                         # print('went past midnight')
                         ping_date += timedelta(days=1)
 
                     prev_timeofday = timeofday
-    
+
                     timestamp = datetimeFromWasspTime(ping_date, timeofday)
                     # print(f'FISHDATA {timestamp:%Y-%m-%d %H:%M:%S.%f}')
                     if timestamp > prev_file_ping_time:
                         alpha *= 1e-3 # convert from dB/km to dB/m
-                        
-                        white_line = np.frombuffer(packet[i:], dtype='uint32', count=N)                
+
+                        white_line = np.frombuffer(packet[i:], dtype='uint32', count=N)
                         i += N*4
-                        
+
                         beam_angle = np.frombuffer(packet[i:], dtype='float32', count=N)
                         i += N*4
-                        
+
                         beam_width = np.frombuffer(packet[i:], dtype='float32', count=N)
                         i += N*4
-        
+
                         data = np.frombuffer(packet[i:], dtype='int16', count=N*M)
                         data = np.reshape(data, (N,M)).T / 128.0 # [dB]
-                        
+
                         # remove the TVG
                         range_m = c * np.arange(data.shape[0]) / (sample_rate * 2)  # [m]
                         range_m[0] += 1e-3 # avoid a range of 0 for tvg reasons
                         tvg = spreading*np.log10(range_m) + 2*alpha*range_m
                         data = (data.T - tvg.T).T
                         beamsToSave = range(N) # save all beams
-                        
+
                         ek60_config.initialise(N) # only gets run the first time
-        
+
                         for beam in beamsToSave:
                             dg = EK60raw(timestamp, data[:,beam])
                             dg.channel = beam+1
@@ -357,46 +372,25 @@ for file in files:
                             dg.absorptioncoefficient = alpha
                             dg.sampleinterval = 1.0/sample_rate # convert from Hz to interval [s]
                             dg.frequency = ek60_config.configtransducer[beam].frequency
-                        
+
                             ek60_config.configtransducer[beam].bwalongship = beam_width[beam]
                             ek60_config.configtransducer[beam].bwathwartship = beam_width[beam]
-                        
+
                             ek60_datagrams.append(dg)
-                            
-                        # accumlate a beam for an echogram
-                        echogram.append(data[:,2]) # beam 2 is the downwards one when there are five.
-                        
-                        # plt.imshow(data, aspect='auto')
-                        # plt.xlabel('Beam number')
-                        # plt.ylabel('Sample number')
-                        # plt.savefig(dataDir.joinpath(f'{file.stem}_{pingno}.png'), bbox_inches='tight', pad_inches=0.1)
-                        # plt.close()
             elif packet_type == 'RAW_SENS':
-                
+
                 if haveValidDate:
                     i = 24
                     (_, timeofday, port, protocol, _, N) = struct.unpack('<QQBBHI', packet[:i])
-                                        
+
                     timestamp = datetimeFromWasspTime(ping_date, timeofday)
                     sens_data = np.frombuffer(packet[i:], dtype='uint8', count=N)
                     if sens_data[0] == ord('$'):
                         ek60_datagrams.append(EK60nmea(timestamp, sens_data.tobytes().decode()))
                     # print(f'RAW_SENS {timestamp:%Y-%m-%d %H:%M:%S.%f}')
-                
-    # make an echogram
-        
-    # ee = np.array(echogram).T
-    # plt.figure()
-    # im = plt.imshow(ee, aspect='auto', extent=(0, ee.shape[1], ee.shape[0]*sample_interval, 0))
-    # cb = plt.colorbar(im)
-    # cb.set_label('Power [dB]')
-    # plt.xlabel('Ping number')
-    # plt.ylabel('Range [m]')
-    # plt.savefig(dataDir.joinpath(f'{file.stem}_echogram_{pingno}.png'), bbox_inches='tight', pad_inches=0.1)
-    # plt.close()
-    
+
     # Add some info to the datagrams that isn't available until later
-    
+
     # changes from file to file, which is inconvenient in LSSS, so don't do that change
     mean_freq = np.array(sonar_freq).mean()
     mean_bandwidth = np.array(sonar_bandwidth).mean()
@@ -405,10 +399,10 @@ for file in files:
         #     for i in dg.configtransducer:
         #         i.frequency = mean_freq
         if dg.type == 'RAW0':
-            # dg.frequency = mean_freq 
+            # dg.frequency = mean_freq
             dg.bandwidth = mean_bandwidth
             prev_file_ping_time = dg.timestamp
-    
+
     # write out the ek60_datagrams into an EK60 data file.
     first_ping = None
     last_ping = None
@@ -416,12 +410,9 @@ for file in files:
     counter_HDT = 0
     counter_VTG = 0
     every = 20 # only output every every'th NMEA message
-    
-    if results_dir:
-        rawfile = results_dir/(file.name).with_suffix('.raw') # all into one directory
-    else:
-        rawfile = file.with_suffix('.raw') # same dir as wassp file
-    
+
+    rawfile = results_dir/file.with_suffix('.raw')
+
     with open(rawfile, 'wb') as f:
         for dg in ek60_datagrams:
 
@@ -448,13 +439,11 @@ for file in files:
                     dg.write(f)
             else:
                 dg.write(f)
-                
-    print(f'Wrote {len(ek60_datagrams)} EK60 datagrams')
-    
+
+    if verbose:
+        print(f'  Wrote {len(ek60_datagrams)} EK60 datagrams')
+
     # so that LSSS will make a new one
     file.with_suffix('.idx').unlink(missing_ok=True)
-    
-    print(f'First timestamp in file      {ek60_datagrams[0].timestamp:%Y-%m-%d %H:%M:%S.%f}')
-    print(f'First ping timestamp in file {first_ping:%Y-%m-%d %H:%M:%S.%f}')
-    print(f'Last ping timestamp in file  {last_ping:%Y-%m-%d %H:%M:%S.%f}')
-    print(f'Last timestamp in file       {ek60_datagrams[-1].timestamp:%Y-%m-%d %H:%M:%S.%f}')
+
+print(f'Converted {len(files)} file(s).')
